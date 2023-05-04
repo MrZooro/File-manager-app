11b3f9a9bd541d4ad84131affffe57ec63f147f9
LittleDovo4ka <Dorofey0.8.0@gmail.com>
2023-05-05 02:25:51 +0300

    ADD: Splash fragment markup and logic

--- app/src/main/java/com/example/filemanagerapp/view/MainActivity.kt
+++ app/src/main/java/com/example/filemanagerapp/view/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.filemanagerapp
+package com.example.filemanagerapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.filemanagerapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
---
+++ app/src/main/java/com/example/filemanagerapp/view/SplashFragment.kt
@@ -0,0 +1,102 @@
+package com.example.filemanagerapp.view
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.filemanagerapp.BuildConfig
+import com.example.filemanagerapp.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.motionLayoutSplash.setTransitionListener(object : MotionLayout.TransitionListener {
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                binding.startButtonSplash.setOnClickListener{
+                    checkPermissions()
+                }
+            }
+
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int
+            ) {
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int,
+                                            progress: Float
+            ) {
+            }
+
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean,
+                progress: Float
+            ) {
+            }
+
+        })
+
+    }
+
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+                startActivity(
+                    Intent(
+                        Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                        uri
+                    )
+                )
+
+            }
+        } else {
+            val result = ContextCompat.checkSelfPermission(requireContext(),
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+            if (result == PackageManager.PERMISSION_DENIED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(requireContext(), "Storage permission is requires," +
+                            "please allow from settings", Toast.LENGTH_SHORT).show()
+                } else {
+                    requestPermissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(requireContext(), "Permission is granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "Permission is not granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
